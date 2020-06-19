dcd99b23a8e57de1b4a7ad64b5f37ad704fd38d6
Sean Downs <spdowns@protonmail.com>
2020-06-19 09:37:16 -0700

    remove Order object and try the order with just the pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,36 @@
 function Order(pizza, total) {
-  this.pizza = pizza;
-  this.total = total
+
 }
 
 
 
 function Pizza(size, toppings) {
+  this.pizza = pizza;
+  this.total = total
   this.size = size;
   this.toppings = [toppings]
 }
 
 
 
-// Pizza.prototype.toppings() 
+Pizza.prototype.addToppings = function(toppings) {
+  this.toppings.push(toppings)
+}
+
 
 $(document).ready(function() {
   
-  $("#begin-order").click(function() {
-    let Order1 = new Order(pizza, total)
-    $("#begin-order").addClass("hidden")
-    $("#pizza-size").removeClass("hidden"); //Find way to call an outside function to add remove when click
+  $("#begin-order").submit(function() {
+    let pizzaOrder = new Pizza()
+    // $("#begin-order").addClass("hidden")
+    // $("#pizza-size").removeClass("hidden"); //Find way to call an outside function to add remove when click
   });
 
-  $("#size-button").click(function() {
+  // $("#size-button").click(function() {
+    
+  //   $("#pizza-size").addClass("hidden")
+  //   $("#pizza-toppings").removeClass("hidden"); //find way to call an outside function to add remove when click
+  // });
 
-    $("#pizza-size").addClass("hidden")
-    $("#pizza-toppings").removeClass("hidden"); //find way to call an outside function to add remove when click
-  });
 
-  
 });
