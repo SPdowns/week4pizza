162b8d1a18bcebef5be2a85fb17b6da66ca106b6
Sean Downs <spdowns@protonmail.com>
2020-06-19 08:58:47 -0700

    show js changes to instaniate order. thinking of now maybe create js to take in all info and have a button at end to calc pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-function Order() {
+function Order(pizza) {
   this.pizza
 }
 
-function Pizza() {
+function Pizza(size, toppings) {
   this.size
   this.toppings
 }
 
-Pizza.prototype.toppings() 
+// Pizza.prototype.toppings() 
 
 $(document).ready(function() {
+  
   $("#begin-order").click(function() {
+    let Order1 = new Order(pizza)
     $("#begin-order").addClass("hidden")
     $("#pizza-size").removeClass("hidden");
   });
