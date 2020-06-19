84ec575d22cb8e2cc9e1be628edf3ebc8bf1ab73
Sean Downs <spdowns@protonmail.com>
2020-06-19 09:21:42 -0700

    add toppings div and functional button to show it. only topping is cheese

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,32 @@
-function Order(pizza) {
-  this.pizza
+function Order(pizza, total) {
+  this.pizza = pizza;
+  this.total = total
 }
 
+
+
 function Pizza(size, toppings) {
-  this.size
-  this.toppings
+  this.size = size;
+  this.toppings = [toppings]
 }
+
+
 
 // Pizza.prototype.toppings() 
 
 $(document).ready(function() {
   
   $("#begin-order").click(function() {
-    let Order1 = new Order(pizza)
+    let Order1 = new Order(pizza, total)
     $("#begin-order").addClass("hidden")
-    $("#pizza-size").removeClass("hidden");
+    $("#pizza-size").removeClass("hidden"); //Find way to call an outside function to add remove when click
   });
+
+  $("#size-button").click(function() {
+
+    $("#pizza-size").addClass("hidden")
+    $("#pizza-toppings").removeClass("hidden"); //find way to call an outside function to add remove when click
+  });
+
+  
 });
